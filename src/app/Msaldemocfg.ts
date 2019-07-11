fcdd75722cbe2d4522ca66d9e6d20baead8dc91c
David McWee <dmcwee@microsoft.com>
2019-07-11 08:10:33 -0400

    Updates to use hte http-server for start

--- src/app/Msaldemocfg.ts
+++ src/app/Msaldemocfg.ts
@@ -1,4 +1,5 @@
 export const MSALDemoCfg = {
-    clientID:'[YOUR CLIENT ID HERE]',
+    clientID: '[YOUR CLIENT ID HERE]',
+    // authority is not needed if you deploy this as a shared/public client
     authority: "https://login.microsoftonline.com/[YOUR TENANT NAME HERE]"
 }
