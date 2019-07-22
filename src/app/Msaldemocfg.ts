99f81a3cf3d4a80912db53fff53dad2dda959be3
David McWee <dmcwee@microsoft.com>
2019-07-22 13:04:23 -0400

    Update of the app id

--- src/app/Msaldemocfg.ts
+++ src/app/Msaldemocfg.ts
@@ -1,4 +1,4 @@
 export const MSALDemoCfg = {
-    clientID:'7b541988-89df-422d-9fc6-28973ee15bd6'
+    clientID:'edde0913-9eee-4368-9392-69077131cf24'
     //authority: "https://login.microsoftonline.com/m365x729777.onmicrosoft.com"
 }
